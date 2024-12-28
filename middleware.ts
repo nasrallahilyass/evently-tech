af1ed1ba0e5beec3e102abeecb4200fb56916f24
nasrallahilyass <nasrallah.dev@gmail.com>
2024-12-28 14:34:16 +0100

    clerk auth

---
+++ middleware.ts
@@ -0,0 +1,35 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// Define routes that are public (do not require authentication)
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/events/:id",
+  "/api/webhooks/clerk",
+  "/api/webhooks/stripe",
+  "/api/uploadthing",
+]);
+
+// Define routes that are ignored and should not trigger authentication
+const isIgnoredRoute = createRouteMatcher([
+  "/api/webhooks/clerk",
+  "/api/webhooks/stripe",
+  "/api/uploadthing",
+]);
+
+export default clerkMiddleware(async (auth, request) => {
+  // Protect routes that are not public or ignored
+  if (!isPublicRoute(request) && !isIgnoredRoute(request)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
