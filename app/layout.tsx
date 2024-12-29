67b796ec5738fe0de13fefd3de16a708bced952d
nasrallahilyass <nasrallah.dev@gmail.com>
2024-12-29 15:04:09 +0100

    fix layout issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Poppins } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
-export const poppins = Poppins({
+const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   variable: "--font-poppins",
