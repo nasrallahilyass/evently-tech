334c7d4f5332e75adc42ec284aad8dd702b13a27
nasrallahilyass <nasrallah.dev@gmail.com>
2024-12-29 19:32:35 +0100

    Always allow public API webhooks

--- middleware.ts
+++ middleware.ts
@@ -27,6 +27,8 @@ export default clerkMiddleware(async (auth, request) => {
 
 export const config = {
   matcher: [
+    // Always allow public API webhooks
+    "/api/webhooks/:path*",
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
