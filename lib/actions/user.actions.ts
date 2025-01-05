1a81d0bccfe2670084934d92cb665af8036af441
nasrallahilyass <nasrallah.dev@gmail.com>
2025-01-05 13:34:46 +0100

    update user.action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,7 @@
-'use server';
+'use server'
+
 import { revalidatePath } from 'next/cache'
+
 import { connectToDatabase } from '@/lib/database'
 import User from '@/lib/database/models/user.model'
 import Order from '@/lib/database/models/order.model'
